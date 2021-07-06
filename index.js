b872e4bb5e6d8429837709f62dd29c2705c8e5e4
bexearn12 <earnshr@gmail.com>
2021-07-06 14:36:07 -0500

    commit tree 3602062aecc2ee0be634dff2b13ddbd41e8c6db1

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const element = document.querySelector("main#main");
-element.remove();
 
-let newHeader1 = document.createElement("h1");
+document.getElementById("main").remove();
+//removed main element
+let newHeader1 = document.createElement('h1');
+//created new tag <h1>
 document.body.appendChild(newHeader1);
+// appended a child to <h1>
 newHeader1.id = "victory";
-newHeader1.innerHTML = "Shawn is the champion";
+//changed element id = "victory"
+newHeader1.innerHTML = "YOUR-NAME is the champion";
+//added the string of text to the <h1> tag
 const newHeader = document.querySelector("#victory");
+//creates new variable that points to 'h1#victory'
+
+// 1) index.html
+// after index.js is processed
+// no longer has DOM node 'main#main':
+// Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
+//https://www.tutorialspoint.com/remove-element-by-id-in-javascript
+
+// 2) index.html
+// after index.js is processed
+//   has a 'newHeader' variable that points to node 'h1#victory':
+// ReferenceError: newHeader is not defined
+// ✓ no longer has DOM node 'main#main'
+// ✓ has a 'newHeader' variable that points to node 'h1#victory'
+// ✓ has a 'newHeader' variable that points to node 'h1#victory'
+// ✓ has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
+// 3) index.html
+// after index.js is processed
+//   has a 'newHeader' variable that points to node 'h1#victory':
+// ReferenceError: newHeader is not defined
+
+// 4) index.html
+// after index.js is processed
+//   has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside:
+// ReferenceError: newHeader is not defined
